766923c460868b548402069c8d6dbbe2dc62889a
someshj5 <someshj5@gmail.com>
2021-05-16 13:35:20 +0530

    user model and routes added

---
+++ routes/api/post.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const router = express.Router();
+
+// @route  GET  api/post
+// @desc   Test route
+// @access PUBLIC
+router.get("/", (req, res) => {
+  res.send("post route");
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
+
+//Connect Database
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("Api running");
 });
+
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/post", require("./routes/api/post"));
 
 const port = process.env.PORT || 5000;
 
